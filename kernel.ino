0b8b3a880ffc2b3b0051fc4b9af5a8d6e2c43ffd
shengjhe <shengjhe@gmail.com>
2013-07-18 17:03:43 +0800

    修改與遠端伺服器主機相關連線方式
    1.增加登入遠端伺服器主機函數
    2.增加MD5編碼函數
    3.修改原本的sendMessage  發送警訊的機制，如果遺失Cookie 將會重新連線
    4.增加Http  關閉連線函數

--- kernel.ino
+++ kernel.ino
@@ -1,24 +1,52 @@
 #include <Ethernet.h>
 #include <SPI.h>
-int pin =2;
-int hpin=3;
-int signal=0;
-int result=2;
+#include <MD5.h>
+/******************************************************
+**              主機參數設定                        **
+******************************************************/
+int   result=2;
 long  updatatime=0;
 long starttime=0;
-//網路參數宣告
-//設定網路卡位址
-byte mac[] = { 0x00, 0x51, 0x56, 0xAC, 0x31, 0x21 };
+String HDAddress="shengjhe";
 
-byte ipaddr []={192,168,2,77};
+/******************************************************
+**               網路參數設定                        **
+******************************************************/
+
+//設定網路卡位址
+byte mac[] = { 0x00, 0x51, 0x56, 0xAC, 0x31, 0x37 };
+byte ipaddr []={192,168,2,114};
 byte netmask[]={255,255,255,0};
 byte getway[]={192,168,2,1};
 byte googledns[]={168,95,1,1};
 char server[] = "192.168.2.1";
-String localAddress="shengjhe";
-String cookie="";
-
 EthernetClient client;
+
+
+
+/******************************************************
+**               加密參數設定                        **
+******************************************************/
+String md5String="";
+int md5Stringlen=0;
+String sendMD5String="";
+String myhashString="SJProject";
+
+
+/******************************************************
+**              伺服器連線相關參數設定             **
+******************************************************/
+
+String cookie="";
+String randString="";
+String responseMessage="";
+
+
+
+/******************************************************
+**                 初始化設定                        **
+******************************************************/
+
 void setup (){
   Serial.begin(9600);
   pinMode(13,OUTPUT);
@@ -27,11 +55,21 @@ void setup (){
   Serial.println(Ethernet.localIP());
   delay(1000);
   Serial.println("connecting...");
+  
+  Serial.println("Geting cookie");
+  login();
 }
-void loop(){
+
+
+
+/******************************************************
+**           Arduino 循環函數                        **
+******************************************************/
+
+void loop()
+{
   if(Serial.available() >0 || millis() > updatatime)
   {
-    
     char str =Serial.read();
     if(str ==0)
     {
@@ -48,7 +86,8 @@ void loop(){
        result=0;
        sendMessage();
        updatatime=millis()+180000;
-    } else if(str ==0x03 && (millis()-starttime)>3000)
+    } 
+    else if(str ==0x03 && (millis()-starttime)>3000)
     {
         //代表端點剛開機
        result=3;
@@ -62,81 +101,171 @@ void loop(){
       //兩分鐘沒收到端點傳來訊息就送出遺失訊號
       result=2;
       sendMessage();
-      updatatime=millis()+120000;
+      updatatime=millis()+30000;
     }
   }
-
   
 }
 
 
-void sendMessage( )
-{
 
+
+/******************************************************
+**           登入伺服器主機函數                      **
+******************************************************/
+
+void login()
+{
+  delay(1000);
   if (client.connect(server, 8080))
  {
-     if(cookie == "")
-     {
-              Serial.println("connected  no cookie"); 
-              client.print("PUT /TMIProject/api/status/");
-              client.print(localAddress);
-              client.print("/");
-              client.print(result);
+              client.print("GET /TMIProject/APP/Users/login/shengjhe");
               client.println(" HTTP/1.1");
               client.println("Host:192.168.2.1");
               client.println("");
               client.println("Connection: close");
-              Serial.print("The message is send");
-              Serial.println(result);
-                delay(1000);
-  
+              delay(1000);
               while (client.available() >0) 
               {
                     char c = client.read();
-                    cookie = cookie + c; 
-                        if(cookie.endsWith("Set-Cookie: "))
+                    randString += c; 
+                    
+                        if(randString.endsWith("Set-Cookie: "))
                         {
-                           cookie="";
+                           randString="";
                         }
-                        if(cookie.endsWith("; Path"))
+                        if(randString.endsWith("; Path"))
                         {
+                            cookie=randString;
+                            randString="";
                             cookie.replace("; Path", "");
-                            Serial.print(cookie);
-                            break;
                         }
-              }
-     
-     }
-     else 
-     {
-              Serial.println("connected  have cookie"); 
-              Serial.println(cookie);
-              client.print("PUT /TMIProject/api/status/");
-              client.print(localAddress);
-              client.print("/");
-              client.print(result);
-              client.println(" HTTP/1.1");
-              client.println("Host:192.168.2.1");
-              client.print("Cookie:");
-              client.println(cookie);
-              client.println("");
-              client.println("Connection: close");
-              Serial.print("The message is send");
-              Serial.println(result);
-     }
-           
-  } 
-  else
-  {
-    Serial.println("connection failed");
-  }
-  
 
-   while(client.connected()) {
-    delay(50);
-    Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
-  }
+                       if(randString.endsWith("\r\n"))
+                        {
+                            randString="";
+                        }
+  
+              }
+  } 
+  
+            stopHttpConnection();
+            Serial.print("cookie:");
+            Serial.println(cookie);
+            Serial.print("randString:");
+            Serial.println(randString);
+            encryptMD5();
+
 }
 
+
+/******************************************************
+**              MD5加密函數                          **
+******************************************************/
+void encryptMD5()
+{
+  //判斷伺服器回傳回來的亂數不為空
+    if(!randString.equals(""))
+    {
+        //將主機ID + 伺服器回傳的亂數 + 團隊自訂字串彙整
+        md5String = HDAddress+randString+myhashString;
+        //取得加密前字串長度
+        md5Stringlen=md5String.length()+1;
+        char md5char[md5Stringlen];
+        //將彙整完畢的完整字串進行16 byte  加密
+        md5String.toCharArray(md5char,md5Stringlen);
+        unsigned char* hash=MD5::make_hash(md5char);
+        char *md5str= MD5::make_digest(hash, 16);
+        sendMD5String=md5str;
+        Serial.println(sendMD5String);
+    }
+    else
+    {
+        login();
+    
+    }
+
+}
+
+
+/******************************************************
+**           發送警訊通知伺服器函數                  **
+******************************************************/
+
+void sendMessage( )
+{
+      if (client.connect(server, 8080))
+     {
+                   //判斷Cookie不為空才發送Request
+                 if(!cookie.equals(""))
+                 {
+                          client.print("PUT /TMIProject/APP/Status/");
+                          client.print(sendMD5String);  // 傳送加密字串
+                          client.print("/");
+                          client.print(result);   //傳送偵測狀態結果
+                          client.println(" HTTP/1.1");
+                          client.println("Host:192.168.2.1");
+                          client.print("Cookie:");
+                          client.println(cookie);  //傳送Cookie
+                          client.println("");
+                          client.println("Connection: close");
+                          Serial.print("The message is send");
+                          Serial.println(result);
+                 }
+                 else 
+                 {
+                     login();
+                 }
+               
+      } 
+      else
+      {
+        //連線失敗
+        Serial.println("connection failed");
+      }
+      delay(1000);
+      
+      
+      //讀取Http Response Result
+      while (client.available() >0) 
+      {
+          char ch = client.read();
+          responseMessage+=ch;
+  
+          //過濾Http Header
+          if(responseMessage.endsWith("\r\n"))
+          {
+                responseMessage="";
+          }
+      
+      }
+      
+    //判斷如果Http回傳的網頁結果是 Not succesfully，就把cookie 還有亂數清空，讓主機重新進行登入
+    if (responseMessage.equals("Not succesfully"))
+    {
+        responseMessage="";
+        cookie="";
+        randString="";
+    }
+   
+    Serial.println(responseMessage);
+    
+    //關閉Http 連線，並清空 responseMessage 變數內容
+   stopHttpConnection();
+   responseMessage="";
+}
+
+
+/******************************************************
+**                Http 關閉連線函數                  **
+******************************************************/
+void stopHttpConnection()
+{
+    //判斷http 連線是存在的，而且已經把 http response 結果讀取完畢，才進行連線關閉
+     if (!client.connected())
+     {
+          Serial.println();
+          Serial.println("disconnecting.");
+          client.stop();
+    }
+}
